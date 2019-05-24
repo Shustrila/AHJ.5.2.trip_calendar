b00b722608f801597a82bebc6d3070f5764eeb2b
Alexandr Maxalov <sanya.maxalov@gmail.com>
2019-05-24 19:56:40 +0300

    add tests

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-[AHJ.5.2.trip_calendar]()
+[AHJ.5.2.trip_calendar](https://shustrila.github.io/AHJ.5.2.trip_calendar/)
 --
 [![AppVeyor](https://img.shields.io/appveyor/ci/shustrila/ahj-5-2-trip-calendar.svg?logo=appveyor&logoColor=white)](https://ci.appveyor.com/project/Shustrila/ahj-5-2-trip-calendar)
--- e2e/form.spec.js
+++ e2e/form.spec.js
@@ -1,11 +1,12 @@
 import puppetteer from 'puppeteer';
+import moment from 'moment';
 
 jest.setTimeout(20000);
 
 describe('TESTS: form date', () => {
     let browser = null;
     let page = null;
-    const baseUrl = 'https://shustrila.github.io/AHJ.5.3.list_editor/';
+    const baseUrl = 'https://shustrila.github.io/AHJ.5.2.trip_calendar/';
 
     beforeAll(async () => {
         browser = await puppetteer.launch({
@@ -16,8 +17,29 @@ describe('TESTS: form date', () => {
         page = await browser.newPage();
     });
 
-    test('1. error form', async () => {
+    test('1. validate form', async () => {
+        await page.goto(baseUrl);
+        await page.click('.form__submit');
 
+        await page.$(".form form__label", async item => {
+            await item.waitForSelector('.error');
+        });
+    });
+
+
+    test('1. error form', async () => {
+        await page.goto(baseUrl);
+        const widget = await page.$('[data-widget="date-form"]');
+        await widget.click('[name="date-there"]');
+        const dateThere = await moment().add(1, 'd').format('DD');
+        await widget.click(`.form__label-there .calendar [data-day="${dateThere}"]`);
+        await widget.click('[name="date-black"]');
+        const dateBack = await moment().add(2, 'd').format('DD');
+        await widget.click(`.form__label-black .calendar [data-day="${dateBack}"]`);
+        page.on('dialog', async dialog => {
+            await dialog.accept();
+        });
+        await widget.click('.form__submit');
     });
 
     afterAll(async () => {
